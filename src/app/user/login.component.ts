165ab91e4d6fa51e8571f5ab4bab22f924318fa3
CliffordTeneng <cliffordteng5@gmail.com>
2019-11-07 22:22:01 +0100

    implemented the login form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,11 +4,12 @@ import { Router } from "@angular/router";
 
 @Component({
   templateUrl: "./login.component.html",
-  styles: ["em{float:right, color: red, padding-left: 10px}"]
+  styles: ["em{float:right; color: #e05c65; padding-left: 10px;}"]
 })
 export class LoginComponent {
   userName;
   password;
+  mouseoverLogin;
   constructor(private authService: AuthServices, private router: Router) {}
   login(formData) {
     this.authService.loginUser(formData.userName, formData.password);
@@ -17,7 +18,7 @@ export class LoginComponent {
   cancel() {
     this.router.navigate(["/events"]);
   }
-//   check() {
-//     console.log();
-//   }
+  //   check() {
+  //     console.log();
+  //   }
 }
