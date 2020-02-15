0a3b29ec64347d8e2e42a94c52c8f44ae02df2d1
cliffordTeneng <tenengclifford5@gmail.com>
2020-02-15 23:43:42 +0100

    implemented the save button for create event

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,14 +1,18 @@
 import { Component } from "@angular/core";
 import { EventService } from "../shared/event.service";
 import { ActivatedRoute } from "@angular/router";
-import { IEvent } from '../shared';
+import { IEvent, ISession } from '../shared';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 @Component({
   templateUrl: "./create-session.component.html",
-  styles: [
-    
-  ]
+  styles: [`em{float:right; color: #e05c65; padding-left: 10px;} 
+  .error input {background-color:#e3c3c5}
+  .error :: -webkit input placeholder{color:#999}
+  .error :: -moz-placeholder{color:#999}
+  .error : -moz-placeholder{color:#999}
+  .error : ms-input-placeholder{color:#999}`]
 })
 export class CreateSessionComponent {
     name: FormControl
@@ -28,11 +32,25 @@ export class CreateSessionComponent {
       this.abstract = new FormControl("", [Validators.required, Validators.maxLength(400)])
 
       this.newSessionFrom = new FormGroup({
-          name: this.name,
+          sessionName: this.name,
           presenter: this.presenter,
           duration: this.duration,
           level: this.level,
           abstract: this.abstract
       })
   }
+  saveSession(sessionData){
+    let session: ISession = {
+      id: undefined,
+      name: sessionData.sessionName,
+      duration: sessionData.duration,
+      level: sessionData.level,
+      presenter: sessionData.presenter,
+      abstract: sessionData.abstract,
+      voters: []
+    }
+    console.log(session)
+  }
+
+
 }
